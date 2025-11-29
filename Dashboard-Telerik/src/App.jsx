1cdcd2104912858d9f8b3be5cd6217422ffe2821
Galin-Yanev-Posten <galin.yanev@bring.com>
2025-11-29 21:25:17 +0100

    package-lock file and package.json

--- Dashboard-Telerik/src/App.jsx
+++ Dashboard-Telerik/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="bg-slate-700 min-h-screen w-full">
+    <div className="bg-slate-700 test-h-screen w-full">
       <Header />
     </div>
   );
