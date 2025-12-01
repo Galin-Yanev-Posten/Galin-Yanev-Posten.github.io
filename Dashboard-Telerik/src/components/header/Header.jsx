92802d581cf61e19387eef8ac680e645b5b02efd
Daniel Danchev <danyorebel@gmail.com>
2025-12-01 09:42:46 +0200

    Adjusted Dashboard to include stockstable with data

--- Dashboard-Telerik/src/components/header/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <div className="flex items-center justify-center min-h-screen text-2xl">
-      <h1 className="text-white animate-pulse">This project is ready to go!</h1>
-    </div>
-  );
-}
--- Dashboard-Telerik/src/pages/Dashboard.jsx
+++ Dashboard-Telerik/src/pages/Dashboard.jsx
@@ -1,24 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import StocksTable from "../components/StocksList/stocksTable";
 
 export default function Dashboard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="min-h-screen bg-white">
-            <header className="border-b bg-white">
-                <div className="max-w-6xl mx-auto px-6 py-3 flex items-center justify-between">
-                    <div className="text-lg font-semibold">Dashboard app</div>
-                    <button
-                        onClick={() => navigate("/login")}
-                        className="bg-black text-white text-sm px-4 py-2 rounded-md"
-                    >
-                        Log out
-                    </button>
-                </div>
-            </header>
-            <main className="max-w-6xl mx-auto px-6 py-20">
-                <h1>Welcome to Dashboard</h1>
-            </main>
+  return (
+    <div className="min-h-screen bg-white">
+      <header className="border-b bg-white">
+        <div className="max-w-6xl mx-auto px-6 py-3 flex items-center justify-between">
+          <div className="text-lg font-semibold">Dashboard app</div>
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-black text-white text-sm px-4 py-2 rounded-md"
+          >
+            Log out
+          </button>
         </div>
-    );
+      </header>
+      <main className="max-w-6xl mx-auto px-6 py-20">
+        <StocksTable />
+      </main>
+    </div>
+  );
 }
